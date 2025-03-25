9074930f3ce01ba8c8191915db4d29b872ac9f21
Aman Godz <amanpooniaff@gmail.com>
2025-03-25 11:46:21 +0530

    Create flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,15 @@
+# flask_app.py
+from flask import Flask, jsonify
+from config import PORT
+
+app = Flask(__name__)
+
+@app.route("/health")
+def health_check():
+    return jsonify({"status": "OK"}), 200
+
+def run_flask():
+    app.run(host="0.0.0.0", port=PORT)
+
+if __name__ == "__main__":
+    run_flask()
